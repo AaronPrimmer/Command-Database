e9a8358e8dfda3f9b1775a617524872116f508a3
Aaron Primmer <aaronmprimmer@gmail.com>
2026-01-19 08:14:33 -0500

    Added first question

--- databaseCommand.js
+++ databaseCommand.js
@@ -14,6 +14,26 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
+const questions = [
+  {
+    type: "list",
+    name: "action",
+    message: "Choose an action:",
+    choices: [
+      "View All Products",
+      "View Low Inventory",
+      "View All Suppliers",
+      "Add Product",
+      "Add Supplier",
+      "Restock Product",
+      "Record Sale",
+      "Update Product",
+      "Delete Product",
+      "Exit",
+    ],
+  },
+];
+
 console.log(chalk.green("Welcome to the Inventory Management System"));
 
 const result = await pool.query("SELECT * FROM products");
