9472584c2e20c2c90d99524ecaae4c13435a3239
Aaron Primmer <aaronmprimmer@gmail.com>
2026-01-22 07:16:33 -0500

    Add Product Works

--- databaseCommand.js
+++ databaseCommand.js
@@ -2,18 +2,11 @@ import chalk from "chalk";
 import inquirer from "inquirer";
 import { Pool } from "pg";
 import express from "express";
+import promptUser from "./lib/prompt.js";
 import sqlFunctions from "./lib/sql.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
 
 console.log(chalk.green("Welcome to the Inventory Management System"));
 
